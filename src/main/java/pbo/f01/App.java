e1ece48818a6288299af73675b50781c805eb46b
marceljsh <marceljoshua69@gmail.com>
2023-04-18 09:04:19 +0700

    Initial commit

---
+++ src/main/java/pbo/f01/App.java
@@ -0,0 +1,103 @@
+package pbo.f01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import pbo.f01.model.Dorm;
+import pbo.f01.model.Student;
+
+/**
+ * NIM - Nama
+ * NIM - Nama
+ * mvn clean compile assembly:single
+ * java -cp .\target\f01-1.0-SNAPSHOT-jar-with-dependencies.jar pbo.f01.App
+ */
+public class App {
+
+    static final String DATA_SEPARATOR = "#";
+
+    public static void main(String[] _args) {
+        Simulator simulator = new Simulator("dormy_pu");
+        Scanner scanner = new Scanner(System.in);
+        boolean keepGoing = true;
+        String line = null;
+
+        while (scanner.hasNext() && keepGoing) {
+            line = scanner.nextLine();
+
+            if (line.equals("---")) {
+                break;
+            }
+
+            String[] data = line.split(DATA_SEPARATOR);
+            String command = data[0];
+            data = Arrays.copyOfRange(data, 1, data.length);
+
+            switch (command) {
+                case "student-add":
+                    addStudent(simulator, data);
+                    break;
+
+                case "dorm-add":
+                    addDorm(simulator, data);
+                    break;
+
+                case "assign":
+                    assign(simulator, data);
+                    break;
+
+                case "display-all":
+                    displayAll(simulator);
+                    break;
+            }
+        }
+
+        scanner.close();
+        simulator.shutdown();
+    }
+
+    private static void addStudent(Simulator simulator, String[] data) {
+        if (simulator.findStudent(data[0]) != null) {
+            return;
+        }
+
+        simulator.save(new Student(data[0], data[1], data[2], data[3]));
+    }
+
+    private static void addDorm(Simulator simulator, String[] data) {
+        if (simulator.findDorm(data[0]) != null) {
+            return;
+        }
+
+        simulator.save(new Dorm(data[0], Integer.parseInt(data[1]), data[2]));
+    }
+
+    private static void assign(Simulator simulator, String[] data) {
+        Student student = simulator.findStudent(data[0]);
+        if (student == null) {
+            return;
+        }
+
+        Dorm dorm = simulator.findDorm(data[1]);
+        if (dorm == null) {
+            return;
+        }
+
+        if (dorm.addResident(student)) {
+            simulator.update(dorm);
+        }
+    }
+
+    private static void displayAll(Simulator simulator) {
+        List<Dorm> dorms = simulator.getAllDorms();
+
+        for (Dorm dorm : dorms) {
+            System.out.println(dorm);
+            for (Student student : dorm.getResidents()) {
+                System.out.println(student);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/pbo/f01/model/Dorm.java
@@ -0,0 +1,111 @@
+package pbo.f01.model;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dorms")
+public class Dorm {
+
+    @Id
+    @Column(name = "name", nullable = false, length = 40)
+    private String name;
+    @Column(name = "capacity")
+    private Integer capacity;
+    @Column(name = "gender", nullable = false, length = 8)
+    private String gender;
+
+    @OneToMany(mappedBy = "dorm", targetEntity = Student.class, cascade = CascadeType.ALL)
+    private Set<Student> residents;
+
+    public Dorm() {
+        // empty
+    }
+
+    public Dorm(String name, Integer capacity, String gender) {
+        this.name = name;
+        this.capacity = capacity;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getResidentNum() {
+        return this.residents.size();
+    }
+
+    public boolean addResident(Student student) {
+        if (!student.getGender().equals(this.gender)) {
+            return false;
+        }
+
+        if (this.getResidentNum() >= this.capacity) {
+            return false;
+        }
+
+        student.setDorm(null);
+
+        return this.residents.add(student);
+    }
+
+    public Set<Student> getResidents() {
+        Set<Student> sortedResidents = new TreeSet<Student>((o1, o2) -> o1.getName().compareTo(o2.getName()));
+        sortedResidents.addAll(residents);
+
+        return sortedResidents;
+    }
+
+    public void setResidents(Set<Student> residents) {
+        this.residents = residents;
+    }
+
+    public boolean hasSpace() {
+        return this.capacity > this.getResidentNum();
+    }
+
+    public boolean isAvailableFor(Student student) {
+        if (this.hasSpace()) {
+            return false;
+        }
+
+        if (!this.gender.equals(student.getGender())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return name + "|" + gender + "|" + capacity + "|" + getResidentNum();
+    }
+}
